75c0d4fafff13efb560d54712ca1686b47efc7ac
alexb <alexb@10.0.1.142>
2016-10-03 15:49:18 +0200

    main modificato

--- 18 RubricaGit/src/rubrica/mainClass.java
+++ 18 RubricaGit/src/rubrica/mainClass.java
@@ -4,7 +4,14 @@ public class mainClass {
 
 	public static void main(String[] args) {
 	
-		
+		int a = 2, b = 3, c = 4;
+		for (int i = 0; i < 100; i++){
+			
+			a = b *c;
+			c = a + b;
+			b = b + b;
+			
+		}
 		
 	}
 
