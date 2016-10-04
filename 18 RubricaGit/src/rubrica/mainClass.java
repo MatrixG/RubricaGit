7f11ae94df4ba5df8c75e1bbcd2134ac4eb3b74d
alexb <alexb@10.0.1.142>
2016-10-04 12:11:21 +0200

    Implementato Main

--- 18 RubricaGit/src/rubrica/mainClass.java
+++ 18 RubricaGit/src/rubrica/mainClass.java
@@ -1,32 +1,42 @@
 package rubrica;
 
+import rubrica.Rubrica;
+
 public class mainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws VocaGi‡Esiste, VoceNonEsiste {
 	
-		do{
-			System.out.print("prova");
-		} while(false) ;
-
-		int a = 2; 
-		int b = 3;
-		int c = 4;
-		for (int i = 0; i < 100; i++){
-			
-			a = b *c;
-			c = a + b;
-			b = b + b;
-			
-		}
+		Rubrica myRub = new Rubrica();
 		
-		System.out.println("Scritto qualcosa");
-		for (int i = 0; i < 100; i++){
-			
-			a = b *c;
-			c = a + c;
-			b = b + b;
-			
-		}
+		
+		//Creo i contatti
+		String nome1 = "Mario";
+		String cognome1 = "Rossi";
+		String tel1 = "331123456";
+		
+		String nome2 = "Andrea";
+		String cognome2 = "Verdi";
+		String tel2 = "3311233333";
+		
+		String nome3 = "Giorgio";
+		String cognome3 = "Giallo";
+		String tel3 = "33112789";
+		
+		
+		//Aggiungo in rubrica
+		myRub.aggiungiVoce(nome1, cognome1, tel1);
+		myRub.aggiungiVoce(nome2, cognome2, tel2);
+		myRub.aggiungiVoce(nome3, cognome3, tel3);
+		
+		//Uso i metodi per cercare ed eliminare
+		Voce miaVoce1 = myRub.getVoce("Giorgio", "Giallo");
+		System.out.println(miaVoce1.getCognome() + " " + miaVoce1.getNome() + " " + miaVoce1.getTelefono());
+		Voce miaVoce2 = myRub.getVoce("Andrea", "Verdi");
+		System.out.println(miaVoce2.getCognome() + " " + miaVoce2.getNome() + " " + miaVoce2.getTelefono());
+		Voce utenteEliminato = myRub.cancellaVoce("Giorgio", "Giallo");
+		System.out.println(utenteEliminato.getCognome() + " " + utenteEliminato.getNome() + " " + utenteEliminato.getTelefono());
+		
+		myRub.getTutteVoci();	
+		
 	}
-
 }
