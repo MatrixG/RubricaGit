f64892dd0b77ad7804a31eebc228b5880d830a7c
Boe92 <boemimarco@yahoo.it>
2016-10-04 12:11:54 +0200

    main

--- 18 RubricaGit/src/rubrica/mainClass.java
+++ 18 RubricaGit/src/rubrica/mainClass.java
@@ -3,30 +3,6 @@ package rubrica;
 public class mainClass {
 
 	public static void main(String[] args) {
-	
-		do{
-			System.out.print("prova");
-		} while(false) ;
-
-		int a = 2; 
-		int b = 3;
-		int c = 4;
-		for (int i = 0; i < 100; i++){
-			
-			a = b *c;
-			c = a + b;
-			b = b + b;
-			
-		}
 		
-		System.out.println("Scritto qualcosa");
-		for (int i = 0; i < 100; i++){
-			
-			a = b *c;
-			c = a + c;
-			b = b + b;
-			
-		}
 	}
-
 }
